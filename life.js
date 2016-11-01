8b64b8810ea779404111146432bcc80a707a85eb
agentpietrucha <agentpietrucha@gmail.com>
2016-11-01 23:51:12 +0100

    final working version. Version 1.0

--- life.js
+++ life.js
@@ -1,18 +1,16 @@
-var recSize = 16;
-var rowsNum = 40;
-var columnsNum = 50;
-var canvas = document.querySelector('#canvas');
-canvas.width = recSize * columnsNum;
-canvas.height = recSize * rowsNum;
-var ctx = canvas.getContext('2d');
-
-
 // 1 cell = 15px x 15px + 1px for border
 /*
 1 cell = 16px x 16px
 Height = 744px
 width = 1301px
 */
+var recSize = 16;
+var rowsNum = 38;
+var columnsNum = 50;
+var canvas = document.querySelector('#canvas');
+canvas.width = recSize * columnsNum;
+canvas.height = recSize * rowsNum;
+var ctx = canvas.getContext('2d');
 //axis parameters
 lineColor = '#f3f3f3'
 var x = 0;
@@ -36,7 +34,6 @@ for(var i = 0; i <= columnsNum; i++){
   ctx.stroke();
   y += recSize;
 }
-
 var aliveCells = new Array;
 canvas.addEventListener('click', function(e){
   var x = e.clientX - canvas.getBoundingClientRect().left;
@@ -64,13 +61,30 @@ function drawRect(x, y){
   y = (Math.floor(y/16) * 16) + .5;
   console.log(x, y);
   ctx.fillRect(x, y, 15, 15);
-  // aliveCells.push(x, y);
 }
-//Each cell with one or no neighbors dies, as if by solitude
-var button = document.querySelector('#button');
+var x;
+function interval(){
+  x = setInterval(live, 1000);
+}
+var interval;
+var clicked = true;
+var button = document.getElementById('playButton');
 button.addEventListener('click', function(e){
-  live();
-
+  if(clicked){
+    interval = setInterval(live, 1000);
+    clicked = false;
+  } else{
+    clearInterval(interval);
+    clicked = true;
+  }
+  if(button.innerText === 'Let it live!'){
+    button.innerText = 'Let it STOP!';
+    button.setAttribute('style', 'width: 83px; left: 84px;');
+  } else{
+    clearInterval(interval);
+    button.innerText = 'Let it live!';
+    button.setAttribute('style', 'width: 67px; left: 92px;');
+  }
 });
 var body = document.body;
 body.addEventListener('keydown', function(e){
@@ -78,13 +92,7 @@ body.addEventListener('keydown', function(e){
     live();
   }
 });
-
 function getLiveNeighboursNum(arr, index) {
-  // START
-  // radze skomitowac to :-)
-  //  tutaj dopisalem i tam nizej tez - zobacz czy skumasz o co kaman,
-  // YOYOYO
-  // debugger;
   var cell = arr[index];
   var liveNum = 0;
   for(var i = 0; i < aliveCells.length; i++){
@@ -101,57 +109,38 @@ function getLiveNeighboursNum(arr, index) {
       liveNum++;
     }
   }
-  console.log('livenum: ', liveNum);
   return liveNum;
-  // END
 }
-var karolek;
 function live(){
-  // debugger;
   var newAliveCells = [];
   for(var i = 0; i < aliveCells.length; i++){
-    // 1. 1-0 neighbors die
-    // 2. 4+ die
-    // 3. 2-3 survive
     var liveNeighboursNum = getLiveNeighboursNum(aliveCells, i);
     if (liveNeighboursNum <= 1 || liveNeighboursNum >= 4) {
-      // aliveCells[i].splice(i, 1); // pewnie splice
       ctx.fillStyle = 'darkcyan';
       ctx.fillRect(aliveCells[i].x + .5, aliveCells[i].y + .5, 14, 14);
       continue;
     } else if (liveNeighboursNum === 2 || liveNeighboursNum === 3) {
-      // continue;
       newAliveCells.push(aliveCells[i]);
     }
   }
   for(var i = 0; i < aliveCells.length; i++){
-    // 4. 3 neighbours gets populated
-    // debugger;
-    var neighbours = getNeighbours(i); //object
+    var neighbours = getNeighbours(i);
     for(var a = 0; a < neighbours.length; a++) {
-      // START
       var neighbour = neighbours[a];
       var liveNeighboursNum = getLiveNeighboursNum(neighbours, a);
       if (liveNeighboursNum === 3) {
-        // add to aliveCells
         ctx.fillStyle = 'aliceblue';
         ctx.fillRect(neighbour.x + .5, neighbour.y + .5, 14, 14);
         if(!containsElement(newAliveCells, neighbour.x, neighbour.y)){
           newAliveCells.push(neighbour);
           }
-        // newAliveCells.push(neighbour);
         continue;
       }
-      // END
     }
   }
-  karolek = newAliveCells;
   aliveCells = newAliveCells;
   }
-
-
 function getNeighbours(index){
   cell = aliveCells[index];
   return [{x: cell.x - 16, y:  cell.y}, {x: cell.x + 16, y: cell.y}, {x: cell.x, y: cell.y - 16}, {x: cell.x, y: cell.y + 16}, {x: cell.x - 16, y: cell.y - 16}, {x: cell.x - 16, y: cell.y + 16}, {x: cell.x + 16, y: cell.y - 16}, {x: cell.x + 16, y: cell.y + 16}];
 }
-// Each cell with one or no neighbors dies, as if by solitude.
