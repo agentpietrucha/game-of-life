fdb519e521ee4cf9872fd1c3471f4977e3324bd4
agentpietrucha <agentpietrucha@gmail.com>
2016-11-12 00:06:43 +0100

    cleaned and organized

--- life.js
+++ life.js
@@ -1,11 +1,11 @@
-// 1 cell = 15px x 15px + 1px for border
 /*
+1 cell = 15px x 15px + 1px for border
 1 cell = 16px x 16px
 Height = 744px
 width = 1301px
 */
-var recSize = 16; //16
-var rowsNum = 38; //38
+var recSize = 16;
+var rowsNum = 38;
 var columnsNum = 50;
 var canvas = document.querySelector('#canvas');
 canvas.width = recSize * columnsNum;
@@ -14,47 +14,43 @@ var ctx = canvas.getContext('2d');
 //axis parameters
 lineColor = '#f3f3f3'
 var x = 0;
-//loop x right
-for(var i = 0; i <= rowsNum; i++){
-  //x axis line
-  ctx.beginPath();
-  ctx.moveTo(0, x);
-  ctx.lineTo(recSize*columnsNum, x);
-  ctx.strokeStyle = lineColor;
-  ctx.stroke();
-  x += recSize;
-}
 var y = 0;
-//loop y down
-for(var i = 0; i <= columnsNum; i++){
-  ctx.beginPath();
-  ctx.moveTo(y, 0);
-  ctx.lineTo(y, recSize*rowsNum);
-  ctx.strokeStyle = lineColor;
-  ctx.stroke();
-  y += recSize;
-}
+//loop x right
+(function drawBoard(){
+  for(var i = 0; i <= rowsNum; i++){
+    //x axis line
+    ctx.beginPath();
+    ctx.moveTo(0, x);
+    ctx.lineTo(recSize*columnsNum, x);
+    ctx.strokeStyle = lineColor;
+    ctx.stroke();
+    x += recSize;
+  }
+  //loop y down
+  for(var i = 0; i <= columnsNum; i++){
+    ctx.beginPath();
+    ctx.moveTo(y, 0);
+    ctx.lineTo(y, recSize*rowsNum);
+    ctx.strokeStyle = lineColor;
+    ctx.stroke();
+    y += recSize;
+  }
+})();
 var aliveCells = new Array;
 canvas.addEventListener('click', function(e){
   var clicked = true;
-  var x = e.clientX - canvas.getBoundingClientRect().left;
-  var y = e.clientY - canvas.getBoundingClientRect().top;
-  drawRect(x, y);
-  x = (Math.floor(x/16) * 16) + .5;
-  y = (Math.floor(y/16) * 16) + .5;
-  if(aliveCells.length === 0){
-    aliveCells.push({x: x, y: y});
-    clicked = !clicked;
-  } else  if(!containsElement(aliveCells, x, y)){
-    aliveCells.push({x: x, y: y});
-    clicked = !clicked;
+  var x = getParameter(e.clientX - canvas.getBoundingClientRect().left);
+  var y = getParameter(e.clientY - canvas.getBoundingClientRect().top);
+  drawRect(x, y, 'aliceblue');
+  if(!containsElement(aliveCells, x, y)){
+      aliveCells.push({x: x, y: y});
+      clicked = !clicked;
     }
     if(clicked){
       for(var i = 0; i < aliveCells.length; i++){
         if(containsElement(aliveCells, x, y)){
           aliveCells.splice(i, 1);
-          ctx.fillStyle = 'darkcyan';
-          ctx.fillRect(x, y, 15, 15);
+          drawRect(x, y, 'darkcyan');
         }
       }
     }
@@ -67,19 +63,16 @@ function containsElement(arr, x, y){
   }
   return false;
 }
-function drawRect(x, y){
-  ctx.fillStyle = 'aliceblue';
-  x = (Math.floor(x/16) * 16) + .5;
-  y = (Math.floor(y/16) * 16) + .5;
-  console.log(x, y);
-  ctx.fillRect(x, y, 15, 15);
+function getParameter(p){
+  var param = (Math.floor(p/recSize) * recSize) + .5;
+  return param;
 }
-var x;
-function interval(){
-  x = setInterval(live, 1000);
+function drawRect(x, y, colour){
+  ctx.fillStyle = colour;
+  console.log(x, y);
+  ctx.fillRect(x, y, recSize - 1, recSize - 1);
 }
 var timeInput = document.querySelector('.timeInput');
-var inpt = document.querySelector('input');
 var interval;
 var clicked = true;
 var playButton = document.getElementById('playButton');
@@ -91,28 +84,25 @@ playButton.addEventListener('click', function(e){
     time = parseInt(timeInput.value);
   }
   if(clicked){
+    playButton.innerText = 'Let it STOP!';
+    playButton.setAttribute('style', 'width: 83px;');
+  } else{
+    clearInterval(interval);
+    playButton.innerText = 'Let it live!';
+    playButton.setAttribute('style', 'width: 67px;');
+  }
+  if(clicked){
     interval = setInterval(live, time);
     clicked = false;
   } else{
     clearInterval(interval);
     clicked = true;
   }
-  if(playButton.innerText === 'Let it live!'){
-    playButton.innerText = 'Let it STOP!';
-    playButton.setAttribute('style', 'width: 83px;');
-    document.querySelector('.playButtonContainer').setAttribute('style', 'left: -8px');
-  } else{
-    clearInterval(interval);
-    playButton.innerText = 'Let it live!';
-    playButton.setAttribute('style', 'width: 67px;');
-    document.querySelector('.playButtonContainer').setAttribute('style', 'left: 0px');
-  }
 });
 var clearButton = document.getElementById('clearButton');
 clearButton.addEventListener('click', function(e){
   for(var i = 0; i < aliveCells.length; i++){
-    ctx.fillStyle = 'darkcyan';
-    ctx.fillRect(aliveCells[i].x, aliveCells[i].y, 15, 15);
+    drawRect(aliveCells[i].x, aliveCells[i].y, 'darkcyan');
   }
   aliveCells = [];
 });
@@ -126,21 +116,19 @@ body.addEventListener('keydown', function(e){
   }
   if(e.keyCode === 13){
     if(clicked){
+      playButton.innerText = 'Let it STOP!';
+      playButton.setAttribute('style', 'width: 83px;');
+    } else{
+      clearInterval(interval);
+      playButton.innerText = 'Let it live!';
+      playButton.setAttribute('style', 'width: 67px;');
+    }
+    if(clicked){
       interval = setInterval(live, time);
       clicked = false;
     } else{
       clearInterval(interval);
       clicked = true;
-    }
-    if(playButton.innerText === 'Let it live!'){
-      playButton.innerText = 'Let it STOP!';
-      playButton.setAttribute('style', 'width: 83px;');
-      document.querySelector('.playButtonContainer').setAttribute('style', 'left: -8px');
-    } else{
-      clearInterval(interval);
-      playButton.innerText = 'Let it live!';
-      playButton.setAttribute('style', 'width: 67px;');
-      document.querySelector('.playButtonContainer').setAttribute('style', 'left: 0px');
     }
   }
 });
@@ -149,14 +137,14 @@ function getLiveNeighboursNum(arr, index) {
   var liveNum = 0;
   for(var i = 0; i < aliveCells.length; i++){
     var c = aliveCells[i];
-    if ( (cell.x - 16 === c.x && cell.y === c.y) ||
-         (cell.x + 16 === c.x && cell.y === c.y) ||
-         (cell.x === c.x && cell.y - 16 === c.y) ||
-         (cell.x === c.x && cell.y + 16 === c.y) ||
-         (cell.x - 16 === c.x && cell.y - 16 === c.y) ||
-         (cell.x - 16 === c.x && cell.y + 16 === c.y) ||
-         (cell.x + 16 === c.x && cell.y - 16 === c.y) ||
-         (cell.x + 16 === c.x && cell.y + 16 === c.y)
+    if ( (cell.x - recSize === c.x && cell.y === c.y) ||
+         (cell.x + recSize === c.x && cell.y === c.y) ||
+         (cell.x === c.x && cell.y - recSize === c.y) ||
+         (cell.x === c.x && cell.y + recSize === c.y) ||
+         (cell.x - recSize === c.x && cell.y - recSize === c.y) ||
+         (cell.x - recSize === c.x && cell.y + recSize === c.y) ||
+         (cell.x + recSize === c.x && cell.y - recSize === c.y) ||
+         (cell.x + recSize === c.x && cell.y + recSize === c.y)
        ) {
       liveNum++;
     }
@@ -168,8 +156,7 @@ function live(){
   for(var i = 0; i < aliveCells.length; i++){
     var liveNeighboursNum = getLiveNeighboursNum(aliveCells, i);
     if (liveNeighboursNum <= 1 || liveNeighboursNum >= 4) {
-      ctx.fillStyle = 'darkcyan';
-      ctx.fillRect(aliveCells[i].x, aliveCells[i].y, 15, 15);
+      drawRect(aliveCells[i].x, aliveCells[i].y, 'darkcyan');
       continue;
     } else if (liveNeighboursNum === 2 || liveNeighboursNum === 3) {
       newAliveCells.push(aliveCells[i]);
@@ -181,8 +168,7 @@ function live(){
       var neighbour = neighbours[a];
       var liveNeighboursNum = getLiveNeighboursNum(neighbours, a);
       if (liveNeighboursNum === 3) {
-        ctx.fillStyle = 'aliceblue';
-        ctx.fillRect(neighbour.x, neighbour.y, 15, 15);
+        drawRect(aliveCells[i].x, aliveCells[i].y, 'aliceblue');
         if(!containsElement(newAliveCells, neighbour.x, neighbour.y)){
           newAliveCells.push(neighbour);
           }
@@ -194,5 +180,8 @@ function live(){
 }
 function getNeighbours(index){
   cell = aliveCells[index];
-  return [{x: cell.x - 16, y:  cell.y}, {x: cell.x + 16, y: cell.y}, {x: cell.x, y: cell.y - 16}, {x: cell.x, y: cell.y + 16}, {x: cell.x - 16, y: cell.y - 16}, {x: cell.x - 16, y: cell.y + 16}, {x: cell.x + 16, y: cell.y - 16}, {x: cell.x + 16, y: cell.y + 16}];
+  return [{x: cell.x - recSize, y:  cell.y}, {x: cell.x + recSize, y: cell.y},
+          {x: cell.x, y: cell.y - recSize}, {x: cell.x, y: cell.y + recSize},
+          {x: cell.x - recSize, y: cell.y - recSize}, {x: cell.x - recSize, y: cell.y + recSize},
+          {x: cell.x + recSize, y: cell.y - recSize}, {x: cell.x + recSize, y: cell.y + recSize}];
 }
